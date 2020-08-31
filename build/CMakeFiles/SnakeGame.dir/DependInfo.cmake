1f1b5b07752492b1b6a7028e5fadd487f9306ce9
Skuzzle-UK <nick@skuzzle.com>
2020-08-31 15:45:00 +0100

    built and tested basic game
    Just performed a cmake .. && make and then run to test original game

---
+++ build/CMakeFiles/SnakeGame.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/hgfs/SharedDocs/GitHub/CppND-Capstone-Snake-Game/src/controller.cpp" "/mnt/hgfs/SharedDocs/GitHub/CppND-Capstone-Snake-Game/build/CMakeFiles/SnakeGame.dir/src/controller.cpp.o"
+  "/mnt/hgfs/SharedDocs/GitHub/CppND-Capstone-Snake-Game/src/game.cpp" "/mnt/hgfs/SharedDocs/GitHub/CppND-Capstone-Snake-Game/build/CMakeFiles/SnakeGame.dir/src/game.cpp.o"
+  "/mnt/hgfs/SharedDocs/GitHub/CppND-Capstone-Snake-Game/src/main.cpp" "/mnt/hgfs/SharedDocs/GitHub/CppND-Capstone-Snake-Game/build/CMakeFiles/SnakeGame.dir/src/main.cpp.o"
+  "/mnt/hgfs/SharedDocs/GitHub/CppND-Capstone-Snake-Game/src/renderer.cpp" "/mnt/hgfs/SharedDocs/GitHub/CppND-Capstone-Snake-Game/build/CMakeFiles/SnakeGame.dir/src/renderer.cpp.o"
+  "/mnt/hgfs/SharedDocs/GitHub/CppND-Capstone-Snake-Game/src/snake.cpp" "/mnt/hgfs/SharedDocs/GitHub/CppND-Capstone-Snake-Game/build/CMakeFiles/SnakeGame.dir/src/snake.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/SDL2"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
